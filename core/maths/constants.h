771ff5fbff10faa938693828dd59044a8e719cdf
Toudonou <hnounagnonoswald@gmail.com>
2024-07-02 23:10:30 +0200

    LIttle update

--- core/maths/constants.h
+++ core/maths/constants.h
@@ -12,7 +12,7 @@ namespace sparky {
     typedef GLfloat real;
 
     inline real toRadian(const real degree) {
-        constexpr real pi_over_180 = std::numbers::pi/ 180; // For optimization
+        constexpr real pi_over_180 = std::numbers::pi / 180; // For optimization
         return degree * pi_over_180;
     }
 }

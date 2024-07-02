e9c1f925e036ed04242d766e2c8353e14b655708
Toudonou <hnounagnonoswald@gmail.com>
2024-07-02 22:50:46 +0200

    Adding mouse getter

--- core/input/InputManager.h
+++ core/input/InputManager.h
@@ -29,7 +29,7 @@ namespace sparky {
 
         [[nodiscard]] bool IsMouseButtonPressed(unsigned int button) const;
 
-        [[nodiscard]] vec2 GetMousePosition() const { return {m_MouseX, m_MouseY}; }
+        __forceinline [[nodiscard]] vec2 GetMousePosition() const { return {m_MouseX, m_MouseY}; }
 
     private:
         static InputManager *s_Instance;
